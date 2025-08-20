8695bb81433921dee11ab3fcc9c7258a0306e6c8
MohdRaza710 <mrmraza.710@gmail.com>
2025-08-20 15:05:36 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import CompilanceRiskManagement from './Screen/Company/CompilanceRiskManagement'
+import PressMedia from './Screen/Company/PressMedia'
 
 function App() {
 
   return (
     <>
-    <CompilanceRiskManagement />
+    <PressMedia />
     </>
   )
 }
