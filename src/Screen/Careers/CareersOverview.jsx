1223d5a1750e09a5ad15cb86b5c19565ffad23c0
MohdRaza710 <mrmraza.710@gmail.com>
2025-09-11 14:49:47 +0300

    changes

--- src/Screen/Careers/CareersOverview.jsx
+++ src/Screen/Careers/CareersOverview.jsx
@@ -9,6 +9,8 @@ import Image100 from '../../assets/image100.png'
 import Image101 from '../../assets/image101.png'
 import Image102 from '../../assets/image102.png'
 import Video1 from '../../assets/video1.mp4'
+import Image103 from '../../assets/Image103.svg'
+import { Button } from 'antd'
 
 // styling left
 function CareersOverview() {
@@ -61,6 +63,61 @@ function CareersOverview() {
         <span className='carer-title1'>Driven by tech. Driven by people.</span>
         <p className='carer-description1'>The transformation of mobility is changing the world. And we are changing with it. Boosted by a strength that has always been anchored in the DNA of our company: the strength to lead. Today, more than ever, we aspire to lead the way and inspire through premium technology. With people who share this desire and wish to join us in shaping the future together. Our Tech Stories introduce these people and show how they work hard every day - from the development of the front camera to the high-voltage battery.</p>
         <a className='carer-link1' href="">Learn more</a>
+      </div>
+
+      <div className='carer-section3'>
+        <span className='carer-title3'>Learn more about the Audi benefits</span>
+        Every individual and employee is different, and so are their lifestyles and preferences. Wherever you live, we want you to feel at home with us.
+      </div>
+
+      <div className='carer-section4'>
+        <div className='carer-grid2'>
+          <div className='carer-card2'>
+            <img src={Image103} alt="" />
+          </div>
+          <div>
+            <Button className='carer-button' type='primary'>To the benefits</Button>
+          </div>
+        </div>
+      </div>
+
+      <div className='carer-section5'>
+        <h1 className='carer-heading5'>Employees share what makes working at Audi special</h1>
+
+        <div className='carer-grid3'>
+
+          <div className='carer-card3'>
+            <img src={Image99} alt="" />
+            <span>We are on fire for Formula 1. With no fossil fuels.</span>
+            <p>Sümeyye, HR Business Partner | Audi Formula Racing GmbH</p>
+            <a href="">Read more </a>
+          </div>
+
+          <div className='carer-card3'>
+            <img src={Image100} alt="" />
+            <span>We work every day to push the boundary between the possible and the impossible.</span>
+            <p>Tiantian, Project Manager Function Development / Application Driver Assistance Systems</p>
+            <a href="">Read more </a>
+          </div>
+
+          <div className='carer-card3'>
+            <img src={Image101} alt="" />
+            <span>An IT revolution is raging in the automotive industry. Now is the time to be a part of it.</span>
+            <p>Sebastian, Head of Delivery Management Digitalization Manufacturing</p>
+            <a href="">Read more </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='carer-section6'>
+        <Button className='carer-emp-button' type='Link'>More employess</Button>
+      </div>
+
+      <div className='carer-section7'>
+        <img className='carer-image7' src={Image102} alt="" />
+        <span className='carer-title7'>Career-related events</span>
+        <p className='carer-description7'>You will receive further information about career opportunities at our events. We are looking forward to seeing you!</p>
+        <Button className='carer-button7' type='Link'>Read more</Button>
       </div>
     </div>
   )
---
+++ src/Screen/Careers/WorkingAtAudi.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image104 from '../../assets/image.png'
+import Image105 from '../../assets/image.png'
+import Image106 from '../../assets/image.png'
+import Image107 from '../../assets/image.png'
+import Image108 from '../../assets/image.png'
+import Image109 from '../../assets/image.png'
+import Image110 from '../../assets/image.png'
+import Image111 from '../../assets/image.png'
+import Image112 from '../../assets/image.png'
+import Image113 from '../../assets/image.png'
+import Image100 from '../../assets/image.png'
+import Image101 from '../../assets/image.png'
+import { Button } from 'antd'
+
+function WorkingAtAudi() {
+  return (
+    <div>WorkingAtAudi</div>
+  )
+}
+
+export default WorkingAtAudi
